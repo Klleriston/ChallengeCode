b7b28693d3ce332ce5ac74fb3d8106dcb99db141
Klleriston Andrade <108811295+Klleriston@users.noreply.github.com>
2024-09-25 16:47:38 -0300

    Update AppleAndOrange.ts

--- AppleAndOrange.ts
+++ AppleAndOrange.ts
@@ -1,22 +1,16 @@
 function countApplesAndOranges(s: number, t: number, a: number, b: number, apples: number[], oranges: number[]): void {
-    //laco iteracao pra s
-    //laco iteracao pra t 
-    
-    //apples array adciona se >=s e <=t 
-    //orange array adiciona se >=t e <=s 
-    
     let orangeCount: number = 0;
     let appleCount: number = 0;
     
-    for (let i = 0; i < apples.length; i++) {
-         if(a + apples[i] >= s && a + apples[i] <= t){
+     for (let i = 0; i < apples.length; i++) {
+        if (a + apples[i] >= s && a + apples[i] <= t) {
             appleCount++;
         }
     }
     
     for (let j = 0; j < oranges.length; j++) {
-        if (b + oranges[j] >= t && j + oranges[j] <= s) {
-            orangeCount++
+        if (b + oranges[j] >= s && b + oranges[j] <= t) {
+            orangeCount++;
         }
     }
     
